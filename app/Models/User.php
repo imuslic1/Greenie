6e82470ff77328c5992442eb1ffe5e74c20945c3
Mirza <mmahmutovi3@etf.unsa.ba>
2025-02-22 23:27:54 +0100

    middleware and super admin command

--- app/Models/User.php
+++ app/Models/User.php
@@ -23,6 +23,7 @@ class User extends Authenticatable
         'password',
         'slug',
         'company_id',
+        'utype',
     ];
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -12,30 +12,25 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/home', [HomeController::class, 'index'])->name('home');
-
-Route::get('/users/{user:slug}', [UserController::class, 'index'])->name('users.index');
-
-Route::post('/connection/{connection}', [ConnectionController::class, 'store'])->name('connection');
-
-Route::get('/leaderboards', [LeaderboardsController::class, 'index'])->name('leaderboards.index');
-
 Route::get('/login', [LoginController::class, 'index'])->name('login');
 Route::post('/login', [LoginController::class, 'store']);
-
 Route::get('/register', [RegisterController::class, 'index'])->name('register');
 Route::post('/register', [RegisterController::class, 'store']);
-
-Route::post('/logout', [LogoutController::class, 'index'])->name('logout');
-
-Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
-
-Route::post('/admin/toggle/offer/{offer_id}', [AdminController::class, 'toggleOfferStatus'])->name('admin.toggleOfferStatus');
-
-Route::post('/admin/toggle/referral_code/{refferalCode_id}', [AdminController::class, 'toggleReferralCodeStatus'])->name('admin.toggleReferralCodeStatus');
-
-
-Route::post('/admin/add/offer', [AdminController::class, 'addOffer'])->name('admin.addOffer');
-
-Route::post('/admin/add/partner', [AdminController::class, 'addPartner'])->name('admin.addPartner');
-
 Route::get('/contact', [HomeController::class, 'contact'])->name('contact');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/users/{user:slug}', [UserController::class, 'index'])->name('users.index');
+    Route::post('/connection/{connection}', [ConnectionController::class, 'store'])->name('connection');
+    Route::get('/leaderboards', [LeaderboardsController::class, 'index'])->name('leaderboards.index');   
+    Route::post('/logout', [LogoutController::class, 'index'])->name('logout');
+});
+
+Route::middleware(['auth', 'admin'])->group(function () {
+    Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
+    Route::post('/admin/toggle/offer/{offer_id}', [AdminController::class, 'toggleOfferStatus'])->name('admin.toggleOfferStatus');
+    Route::post('/admin/toggle/referral_code/{refferalCode_id}', [AdminController::class, 'toggleReferralCodeStatus'])->name('admin.toggleReferralCodeStatus');
+    Route::post('/admin/add/offer', [AdminController::class, 'addOffer'])->name('admin.addOffer');
+    Route::post('/admin/add/partner', [AdminController::class, 'addPartner'])->name('admin.addPartner');
+});
+
+
