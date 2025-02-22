945783d515530963e8eda65e9ebd0e5b641dba04
Mirza <mmahmutovi3@etf.unsa.ba>
2025-02-22 21:48:02 +0100

    schedule task for checking user activity and updating user streak

---
+++ app/Console/Commands/UsersCheckStreakDaily.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+
+class UsersCheckStreakDaily extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'user:check-streak';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Check if users had any activity during the day and end the streak if not';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $yesterday = Carbon::yesterday();
+
+        $inactiveUsers = User::whereDate('last_streak_update', '!=', $yesterday)->get();
+
+        foreach ($inactiveUsers as $user) {
+            Log::info("User {$user->id} was inactive on {$yesterday->toDateString()}.");
+
+            $user->current_streak = 0;
+            $user->save();
+        }
+
+        $this->info('User activity check completed.');
+    }
+}
