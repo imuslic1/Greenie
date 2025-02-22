5bd89257422da82d5bdf584c0f83d9e1a15108ae
Ismar Muslić <ismar.muslic@outlook.com>
2025-02-22 03:05:33 +0100

    Prettied up layout, still static

--- resources/views/leaderboards.blade.php
+++ resources/views/leaderboards.blade.php
@@ -4,44 +4,45 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dropdown Menu</title>
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
 </head>
 <body>
     <div class="container mt-5">
-        <h2>Bootstrap Dropdown</h2>
-        <div class="dropdown">
-            <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
-                Select Option
-            </button>
-            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                <li><a class="dropdown-item" href="{{ route('companies.index') }}">Companies</a></li> 	<!--DODATI ACTUAL PATH DO OVOGA, POPRAVITI companies.index-->
-                <li><a class="dropdown-item" href="{{ route('users.index') }}">Users</a></li>			<!--DODATI ACTUAL PATH DO OVOGA, POPRAVITI users.index-->
-            </ul>
-        </div>
-        <ol class="list-group list-group-numbered mt-5">
-            <li class="list-group-item d-flex justify-content-between align-items-start">
-              <div class="ms-2 me-auto">
-                <div class="fw-bold">Subheading</div>
-                Content for list item
-              </div>
-              <span class="badge text-bg-primary rounded-pill">14</span>
-            </li>
-            <li class="list-group-item d-flex justify-content-between align-items-start">
-              <div class="ms-2 me-auto">
-                <div class="fw-bold">Subheading</div>
-                Content for list item
-              </div>
-              <span class="badge text-bg-primary rounded-pill">14</span>
-            </li>
-            <li class="list-group-item d-flex justify-content-between align-items-start">
-              <div class="ms-2 me-auto">
-                <div class="fw-bold">Subheading</div>
-                Content for list item
-              </div>
-              <span class="badge text-bg-primary rounded-pill">14</span>
-            </li>
-          </ol>
+		<h2 class="mb-5 text-center">LEADERBOARDS</h2>
+		<div class="row">
+			<div class="col md-6">
+				 <h3>Users</h3>
+                <ol class="list-group list-group-numbered">
+                    <li class="list-group-item d-flex justify-content-between align-items-center gold-bg">
+                        <span>Item 1</span>
+                        <img src="{{ asset('images/medal-gold.svg') }}" alt="icon" style="height: 100%; max-height: 30px; width: auto;">
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between align-items-center silver-bg">
+                        <span>Item 2</span>
+                        <img src="{{ asset('images/medal-silver.svg') }}" alt="icon" style="height: 100%; max-height: 30px; width: auto;">
+                    </li>
+					<li class="list-group-item d-flex justify-content-between align-items-center bronze-bg">
+                        <span>Item 2</span>
+                        <img src="{{ asset('images/medal-bronze.svg') }}" alt="icon" style="height: 100%; max-height: 30px; width: auto;">
+                    </li>
+                </ol>
+			</div>
+
+			<div class="col md-6">
+				 <h3>Companies</h3>
+                <ol class="list-group list-group-numbered">
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                        <span>Item 1</span>
+                        <img src="{{ asset('images/medal-gold.svg') }}" alt="icon" style="height: 100%; max-height: 30px; width: auto;">
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                        <span>Item 2</span>
+                        <img src="{{ asset('images/medal-silver.svg') }}" alt="icon" style="height: 100%; max-height: 30px; width: auto;">
+                    </li>
+                </ol>
+			</div>
+		</div>
     </div>
 
     <script src="{{ asset('js/app.js') }}"></script>
