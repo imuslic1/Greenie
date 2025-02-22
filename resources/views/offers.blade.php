37b77d462abf35d207469f9f29e2d9182a5a000b
Ismar Muslić <ismar.muslic@outlook.com>
2025-02-22 23:30:42 +0100

    Working on layout

--- resources/views/offers.blade.php
+++ resources/views/offers.blade.php
@@ -2,52 +2,60 @@
 
 @section('content')
     <div class="container mt-5">
-        <div class="row pb-4">
-            <h2 class="mb-3">Offers</h2>
-            <div class="col-9">
-                <div style="max-height: 40vh; overflow-y: auto;">
-                    <table class="table table-bordered table-hover">
-                        <thead>
-                            <tr>
-                                <th onclick="sortTable(0)" style="cursor:pointer;">Partner</th>
-                                <th onclick="sortTable(1)" style="cursor:pointer;">Tokens needed</th>
-                                <th onclick="sortTable(2)" style="cursor:pointer;">Discount (%)</th>
-                                <th onclick="sortTable(3)" style="cursor:pointer;">Active</th>
-                                <th>Actions</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                @foreach ($partner->offers as $offer)
-                            <tr>
-                                <td>{{ $offer->partner->name }}</td>
-                                <td>{{ $offer->amount }}</td>
-                                <td>{{ $offer->discount_percentage }}</td>
-                                <td>{{ $offer->active ? 
-                                    'Yes'
-                                    : 'No' }}</td>
-                                <td>
-                                    @if ($offer->active)
-                                        <form action="/admin/toggle/offer/{{ $offer->id }}" method="POST"
-                                            style="display:inline;">
-                                            @csrf
-                                            <button type="submit" class="btn btn-secondary">Deactivate</button>
-                                        </form>
-                                    @else
-                                        <form action="/admin/toggle/offer/{{ $offer->id }}" method="POST"
-                                            style="display:inline;">
-                                            @csrf
-                                            <button type="submit" class="btn btn-success">Activate</button>
-                                        </form>
-                                    @endif
-                                </td>
-                            </tr>
-                            @endforeach
-                            </tr>
-                        </tbody>
-                    </table>
+        <div class="row mt-3">
+            <div class="row pb-4">
+                <h2 class="mb-3">Offers</h2>
+                <div class="col-9">
+                    <div style="max-height: 40vh; overflow-y: auto;">
+                        <table class="table transakcije-tabela table-hover">
+                            <thead>
+                                <tr>
+                                    <th onclick="sortTable(0)" style="cursor:pointer;">Partner</th>
+                                    <th onclick="sortTable(1)" style="cursor:pointer;">Tokens needed</th>
+                                    <th onclick="sortTable(2)" style="cursor:pointer;">Discount (%)</th>
+                                    <th onclick="sortTable(3)" style="cursor:pointer;">Active</th>
+                                    <th>Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    @foreach ($partner->offers as $offer)
+                                    <tr>
+                                        <td>{{ $offer->partner->name }}</td>
+                                        <td>{{ $offer->amount }}</td>
+                                        <td>{{ $offer->discount_percentage }}</td>
+                                        <td>
+                                            @if ($offer->active)
+                                                <img src="{{ asset('images/activated.png') }}" alt="Active" width="20px">
+                                            @else
+                                            <img src="{{ asset('images/not-activated.png') }}" alt="Active" width="20px">
+                                            @endif
+
+                                        </td>
+                                        <td>
+                                            @if ($offer->active)
+                                                <form action="/admin/toggle/offer/{{ $offer->id }}" method="POST"
+                                                    style="display:inline;">
+                                                    @csrf
+                                                    <button type="submit" class="btn btn-secondary">Deactivate</button>
+                                                </form>
+                                            @else
+                                                <form action="/admin/toggle/offer/{{ $offer->id }}" method="POST"
+                                                    style="display:inline;">
+                                                    @csrf
+                                                    <button type="submit" class="btn btn-success">Activate</button>
+                                                </form>
+                                            @endif
+                                        </td>
+                                    </tr>
+                                    @endforeach
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
-            </div>
+    
+        </div>
     </div>
 
     <script>
