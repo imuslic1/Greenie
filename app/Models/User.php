840419cf41941435c0170332deb241c71e021d5b
Mirza <mmahmutovi3@etf.unsa.ba>
2025-02-22 22:08:27 +0100

    showing unused referral codes on account page

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,6 +45,11 @@ class User extends Authenticatable
         return $this->belongsTo(Company::class);
     }
 
+    public function referralCodes()
+    {
+        return $this->hasMany(RefferalCode::class);
+    }
+
     /**
      * Get the attributes that should be cast.
      *
