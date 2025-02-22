94cf610a1aabda12e6bf56b372ac5ed8e3c3da39
Haris Mališević <malisevic.haris@gmail.com>
2025-02-22 04:47:23 +0100

    User account page outline done

--- resources/views/account_page.blade.php
+++ resources/views/account_page.blade.php
@@ -1,38 +1,82 @@
 @extends('layouts.base')
 
 @section('content')
-    <div class="m-4">
+    <div class="m-4 mx-auto" style="max-width: 1000px;">
 
         <div class="row">
             <div class="col-auto bg-dark" style="min-width: 300px; min-height: 200px;">
                 <div class="text-light">
 
-                    <h2 class="pt-2">Username</h2>
-                    <h4 class="pb-3 pt-2">Balance: 0 tokens</h4>
-                    <p>email: example@mail.com</p>
+                    <h2 class="pt-2">{{ $user->name }}</h2>
+
+                    <p>{{ $user->email }}</p>
+
+                    <h4 class="pb-3 pt-2"> {{ $user->balance . ' tokens' }}</h4>
+
 
                 </div>
             </div>
+
             <div class="col bg-light">
                 <h2 class="text-center h-25">User stats</h2>
                 <div class="row g-2 h-75" style="border: 1px solid black;">
                     <div class="col text-center d-flex flex-column justify-content-center">
-                        <h2>123</h2>
-                        <p>Total CO2 emission saved</p>
+                        <h2 style="color: {{ $trend > 0 ? 'green' : ($trend < 0 ? 'red' : 'black') }};">{{ $trend . '%' }}
+                        </h2>
+                        <p>This weeks trend</p>
                     </div>
+
                     <div class="col text-center d-flex flex-column justify-content-center">
-                        <h2>11</h2>
+                        <div class="d-flex justify-content-center">
+                            <img src="{{ asset('images/fire-icon.png') }}" alt="Lit!" style="max-width: 50px;">
+                        </div>
+                        <h2>{{ $user->current_streak }}</h2>
                         <p>Current streak</p>
                     </div>
+
                     <div class="col text-center d-flex flex-column justify-content-center">
-                        <h2>17</h2>
+                        <h2>{{ $user->longest_streak }}</h2>
                         <p>Longest streak</p>
                     </div>
                 </div>
             </div>
 
-            <div class="row g-2 mt-4">
-                <div class="col-12">
+
+        </div>
+
+        <div class="row mt-3">
+            <div class="col-auto" style="min-width: 300px; min-height: 200px;">
+                <div>
+
+                    @if ($connections->isEmpty())
+                        <p>No connections</p>
+                    @else
+                        <table class="table table-bordered text-center">
+                            <thead>
+                                <tr>
+                                    <th>Connections</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($connections as $connection)
+                                    <tr>
+                                        <td><a href="/users/{{ $connection->slug }}">{{ $connection->name }}</a>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    @endif
+
+                </div>
+            </div>
+
+            @if ($user->transactions->isEmpty())
+                <div class="col-8 text-center mt-4">
+                    <p>No transactions</p>
+                </div>
+            @else
+                <div class="col">
                     <table class="table table-bordered text-center">
                         <thead class="position-sticky top-0 bg-light">
                             <tr>
@@ -42,22 +86,18 @@
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td>Data 1</td>
-                                <td>Data 2</td>
-                                <td>Data 3</td>
-                            </tr>
-                        </tbody>
-                        <tbody>
-                            <tr>
-                                <td>Data 1</td>
-                                <td>Data 2</td>
-                                <td>Data 3</td>
-                            </tr>
+                            @foreach ($user->transactions as $transaction)
+                                <tr>
+                                    <td>{{ $transaction->partner->name }}</td>
+                                    <td>{{ $transaction->amount }}</td>
+                                    <td>{{ $transaction->updated_at }}</td>
+                                </tr>
+                            @endforeach
                         </tbody>
                     </table>
                 </div>
-            </div>
+            @endif
 
         </div>
+
     @endsection
