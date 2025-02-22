63fdbcfb18ebfd8d6b657a14fe026178922285ad
Mirza <mmahmutovi3@etf.unsa.ba>
2025-02-22 16:23:21 +0100

    icon and title to base blade with fixed routes

--- routes/web.php
+++ routes/web.php
@@ -8,19 +8,12 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/home', [HomeController::class, 'index'])->name('home');
 
 Route::get('/users/{user:slug}', [UserController::class, 'index'])->name('users.index');
 
-Route::get('/account', function () {
-    return view('account_page');
-});
-
 Route::get('/leaderboards', [LeaderboardsController::class, 'index'])->name('leaderboards.index');
-
-Route::get('/home', [HomeController::class, 'index'])->name('home');
 
 Route::get('/login', [LoginController::class, 'index'])->name('login');
 Route::post('/login', [LoginController::class, 'store']);
